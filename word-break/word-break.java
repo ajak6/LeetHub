bfbbe3d25ddbab436507dbaf4e82b7faf0b41c80
Amey Jain <ajak6@users.noreply.github.com>
2021-04-18 23:41:06 -0700

    Time: 16 ms (5.77%), Space: 45.2 MB (6.37%) - LeetHub

--- word-break/word-break.java
+++ word-break/word-break.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean wordBreak(String s, List<String> wordDict) {
+    public boolean wordBreak1(String s, List<String> wordDict) {
         if(s.isBlank())return true;
         //word dict
         //you have dict in hashset
@@ -16,12 +16,43 @@ class Solution {
             }
             
         }
-        map.put(s, false);
+       map.put(s, false);
        return false;
     }
     HashMap<String, Boolean> map = new HashMap<>();
     // on every call you need to find that a string can be normalized or not if we store the string result that should be enough
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> set = new HashSet<>(wordDict);
+        boolean dp [] = new boolean[s.length()];
+        // dp[0]=true; // 0 to 0 is true for blank string // what is blank should also be present in the dict to be valuie
+        //still we will consider this is as a base case if the first  input isblank then return is as is.
+        
+        
+        
+        // dp[i] represent whether 0,i can be broken into valid words
+        for(int i=0;i < s.length(); i++){
+            
+            for(int j =0; j <=i; j++) {
+                
+                String sub = s.substring(j, i +1); //check every char from 0 to i whether that string is present ort not
+                //if present the nthe dp befrore j shopuild betrue
+                
+                if(set.contains(sub) && (j==0 ||dp[j-1]) ) {
+                    dp[i]=true;
+                }
+                
+            }
+        }
+        return dp[dp.length-1];
+    }
     
+    
+    //can we do bottom up if we want to results we will not know which is the complete cain of words that are broken to find that it is true.
+    
+    //every string can only be brokjen when the previous indedx is valid and can be brokenm in top valid stirng.
+    // so you check a character
+        
+        // dp[i][j] means characters starting at i and ending at j are valid when  
     
     
 }
