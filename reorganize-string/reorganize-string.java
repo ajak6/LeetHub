0400bae5f33ef7afbdacb15b3c7dc0a1fc46edc9
Amey Jain <ajak6@users.noreply.github.com>
2021-04-25 17:07:17 -0700

    Time: 2 ms (73.07%), Space: 37.1 MB (68.73%) - LeetHub

--- reorganize-string/reorganize-string.java
+++ reorganize-string/reorganize-string.java
@@ -1,6 +1,40 @@
 class Solution {
     public String reorganizeString(String S) {
-      
+     
+        
+        //need to be greedy to avoid conflicts 
+        
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((x,y) -> Integer.compare(y[1], x[1]));
+        int count[]= new int[26];
+        for(int i =0; i < S.length(); i++){
+            count[S.charAt(i)-'a']++;
+        }
+        
+        for(int i =0; i < count.length; i++) {
+            if(count[i]>0)
+            maxHeap.offer(new int[]{i+'a', count[i]});
+        }
+        
+        int prev[] = null;
+        StringBuilder builder = new StringBuilder();
+        while(!maxHeap.isEmpty()) {
+            int[] temp = maxHeap.poll();
+            
+            builder.append((char)temp[0]);
+            temp[1]--;
+            if(prev!=null){
+                if(prev[0]==temp[0]) return "";
+                if(prev[1]>0)            
+                    maxHeap.offer(prev);
+            }
+            prev = temp;
+        }
+       // System.out.println("builder "+builder.toString());
+        return builder.length() == S.length()? builder.toString():"";
+    }
+                                  
+                                  
+        
         //find out which characters are together?
         //or using all characters create a string in which no chars are repeated
         /*
@@ -12,46 +46,46 @@ class Solution {
         a - 1 - reduce count. 
         total count is zero
         */
-        int count[] = new int[26];
         
         
-        char prev='z';
-        HashMap<Character, Integer> freqCount = new HashMap<>();
-        for(char c: S.toCharArray()) {
-            freqCount.merge(c, 1, Integer::sum);
-        }
         
-        PriorityQueue<Pair> maxHeap = new PriorityQueue<>((p1, p2) -> p2.val - p1.val);
+//         char prev='z';
+//         HashMap<Character, Integer> freqCount = new HashMap<>();
+//         for(char c: S.toCharArray()) {
+//             freqCount.merge(c, 1, Integer::sum);
+//         }
         
-        for(Map.Entry<Character,Integer> entry: freqCount.entrySet()){
-            maxHeap.offer(new Pair(entry.getKey(), entry.getValue()));
-        }
+//         PriorityQueue<Pair> maxHeap = new PriorityQueue<>((p1, p2) -> p2.val - p1.val);
         
-        StringBuilder builder = new StringBuilder();
-        //sort ick the highestfrequenc first and create array
-        // sort by frequency
-        // Heap 
-        Pair entryPrev = null;
-        System.out.println("maxheap  :" +maxHeap);
-        while( maxHeap.size() > 0 ) {
-            // System.out.println("maxheap  :" +maxHeap);
-            Pair curr= maxHeap.poll();
-            // System.out.println("Curr is "+ curr.key);
-            builder.append(curr.key);
-            if(entryPrev !=null && entryPrev.key == curr.key){
-                return "";
-            }
-            if(entryPrev != null && entryPrev.val>0) {
-                maxHeap.offer(entryPrev);
-            }
-            // if(curr.getValue() >0)
-            entryPrev  = new Pair(curr.key, curr.val-1);
+//         for(Map.Entry<Character,Integer> entry: freqCount.entrySet()){
+//             maxHeap.offer(new Pair(entry.getKey(), entry.getValue()));
+//         }
+        
+//         StringBuilder builder = new StringBuilder();
+//         //sort ick the highestfrequenc first and create array
+//         // sort by frequency
+//         // Heap 
+//         Pair entryPrev = null;
+//         // System.out.println("maxheap  :" +maxHeap);
+//         while( maxHeap.size() > 0 ) {
+//             // System.out.println("maxheap  :" +maxHeap);
+//             Pair curr= maxHeap.poll();
+//             // System.out.println("Curr is "+ curr.key);
+//             builder.append(curr.key);
+//             if(entryPrev !=null && entryPrev.key == curr.key){
+//                 return "";
+//             }
+//             if(entryPrev != null && entryPrev.val>0) {
+//                 maxHeap.offer(entryPrev);
+//             }
+//             entryPrev  = new Pair(curr.key, curr.val-1);
             
-        }
-        if(entryPrev!=null && entryPrev.val >0) return "";//this means we have not included something 
-        return builder.toString();    
+//         }
+//         // if(entryPrev!=null && entryPrev.val >0) return "";//this means we have not included something 
+//         String result = builder.toString();
+//         return result.length() == S.length()? result: "";    
         
-    }
+    // }
 }
 class Pair{
     public char key;
