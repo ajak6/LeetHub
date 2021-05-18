cdb317aba8c38fac663c58740d5152420b58bf02
Amey Jain <ajak6@users.noreply.github.com>
2021-05-17 23:31:41 -0700

    Time: 33 ms (46.95%), Space: 48 MB (89.82%) - LeetHub

--- implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -1,59 +1,69 @@
-class Trie {
-    Map<Character, Trie> nodes;
+class TrieNode{
+    char val;
+    TrieNode[] nodes;
     boolean isWord;
+    public TrieNode(){
+        nodes = new TrieNode [26];
+    }
+    public TrieNode getNode(char c){
+        return nodes[c-'a'];
+    }
+    public TrieNode addNode(char c){
+        if(!isPresent(c)){
+            nodes[c-'a']=new TrieNode();
+        }
+        return nodes[c - 'a'];
+    }
+    public boolean isPresent(char c){
+        return nodes[c-'a']!=null;
+    }
+}
+class Trie {
+    // Map<Character, Trie> nodes;
+    TrieNode root;
+    ;
     /** Initialize your data structure here. */
     public Trie() {
-        nodes = new HashMap<>();
+        root = new TrieNode(); 
+        //let this be the root node which stores the trieNode for first character
     }
     
     /** Inserts a word into the trie. */
     public void insert(String word) {
-        Map<Character, Trie> tempNodes = nodes;
-        for(int i =0; i < word.length(); i++) {
+        TrieNode tempRoot = root;
+        for (int i=0; i < word.length(); i++) {
             char c = word.charAt(i);
-            Trie n = new Trie();
-            if(!tempNodes.containsKey(c)){
-                tempNodes.put(c,n);
-            }
-            n=tempNodes.get(c);
-            tempNodes = n.nodes;
-            if(i==word.length()-1){
-                n.isWord=true;
-            }
+            tempRoot = tempRoot.addNode(c);
         }
-        
+        tempRoot.isWord=true;
     }
     
     /** Returns if the word is in the trie. */
     public boolean search(String word) {
-        if(word.isEmpty())return false;
-        // System.out.println("Search of "+ word);
-        Map<Character, Trie> tempNodes = nodes;
-        Trie node = null;
-        for(int i=0; i < word.length(); i++) {
-            char c = word.charAt(i);
-            node = tempNodes.get(c);
-            // System.out.println("char "+ c + " node "+ node.nodes);
-            if(node == null) return false;
-            tempNodes = node.nodes;
-        }
-        return node.isWord;
+        TrieNode tempRoot = root;
+       for (int i=0; i < word.length(); i++) {
+            char c= word.charAt(i);
+            if(tempRoot.isPresent(c)){
+              tempRoot=tempRoot.getNode(c);  
+            } else{
+                return false;    
+            }
+       }
+        return tempRoot.isWord;
     }
     
     /** Returns if there is any word in the trie that starts with the given prefix. */
     public boolean startsWith(String prefix) {
-        Trie node = null;
-        Map<Character, Trie> tempTrie = nodes;
-        for(int i =0; i < prefix.length(); i++) {
+        TrieNode tempRoot = root;
+        for (int i=0; i < prefix.length(); i++) {
             char c = prefix.charAt(i);
-            if(tempTrie.containsKey(c)){
-                node = tempTrie.get(c);
-                tempTrie = node.nodes;
-            } else {
+            if(tempRoot.isPresent(c)){
+                tempRoot=tempRoot.getNode(c);
+            } else{
                 return false;
-            }
+            }    
         }
-        return node != null;
+        return tempRoot!=null;
     }
 }
 
