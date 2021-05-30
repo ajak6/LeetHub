2472bfdf127fafbf80df0e2ce630dad334e1ed20
Amey Jain <ajak6@users.noreply.github.com>
2021-05-29 18:42:48 -0700

    Time: 24 ms (6.01%), Space: 39.2 MB (45.85%) - LeetHub

--- longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.java
+++ longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.java
@@ -1,31 +1,51 @@
 class Solution {
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
-        if(k==0)return 0;
-        int l=0,r=0,max=0;
-        int c[] = new int[128];
-        while(r< s.length()) {
-            
-            char atr = s.charAt(r);
-            if(++c[atr]==1) {
-                k--; // we found a unioque char that is included in the sub
-                if(k<0){
-                    //we have included more than allowed chars.
-                    //we will remove some chars to keep the window valid.
-                    
-                    while(l < r){
-                        char atl = s.charAt(l++);
-                        if(--c[atl]==0){
-                            k++;
-                            break;
-                        }
-                        
-                    }
-                }
-            
+        // at moost k characters.
+        // 2 pointer.
+        // increase r, add to set
+        // if set size >k count the lenfth r-l
+        // keep increasing R and removing the count of chars from Map
+        // when the size is below
+        // or we store te latest index of char at l and move l 1 that index. but then we have to create a new set as well rmeov all chars between l and r 
+        // 
+        // Map of char and last index
+        // we have to clean up the set as well so we can't just remove hte chars/
+        int l=0;
+        int r=0;
+        if(k==0|| s==null || s.isEmpty())return 0;
+        //store last index of character
+        Map<Character,Integer> map = new HashMap<>();
+        // map.put(s.charAt(0),0);
+        int max = 0;
+        char ch[] = s.toCharArray();
+        while(r <= s.length()) {
+            if(r<s.length()){
+                map.put(s.charAt(r), r);
+                r++;
             }
-            max = Math.max(max, r-l+1);
-            r++;
+            if(map.size()<=k) {
+                max = Math.max(r-l, max);
+            }
+            if(map.size()>k) {
+                char atL = s.charAt(l);     
+                //since anything we remove starts at l, atleast this characters needs to go
+                // but we also need toremove all chars lower than its index in the map.
+                //also keep doing until the isze of the map < k
+                 //you have to pick just 1 char and remove it and remove few more that were in the way. so iterator over K dist in amp vs the string which could be higher.
+                int upto = map.get(atL); //charAT L last occurance is upto remove all chars before that
+                //end up removeing more than needed characters.
+                //find the min index stored and move l to that min+1
+                // map.entrySet().removeIf(entr-> entr.getValue()<= upto);
+                int min = (int) map.values().stream().min(Comparator.naturalOrder()).get();
+                map.remove(s.charAt(min));
+                l = min+1;
+            }
+            if(r==s.length()) break;
+            
         }
         return max;
+        
+        
+        
     }
 }
