248fc8fd23e75560da2964f3edeebc566a2b6a1c
Amey Jain <ajak6@users.noreply.github.com>
2021-08-18 14:20:30 -0700

    Time: 0 ms (100.00%), Space: 39.4 MB (42.75%) - LeetHub

--- subsets/subsets.java
+++ subsets/subsets.java
@@ -9,26 +9,39 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         
         // loop and find all combinations of a lenght from the list
-        for(int len =1; len < nums.length+1; len++){
-            findSubSetsOfLength(nums, result, 0, len, new LinkedList<>());
-        }
-        result.add(new ArrayList<>());
+        //make subsets of len 1 then len 2 etc
+        
+        findSubSetsOfLength(nums, result, 0, new LinkedList<>());
+        
+        
         return result;
     }
-    
-    public void findSubSetsOfLength(int nums[], List<List<Integer>> result, int start, int max, LinkedList<Integer> current){
-        
-        if(current.size()==max){
+    public void findSubSetsOfLength(int nums[], List<List<Integer>> result, int start, LinkedList<Integer> current){
+        if(start==nums.length){
             result.add(new ArrayList<>(current));
             return;
         }
-        
-        for(int i=start; i< nums.length;i++) {
-            current.addLast(nums[i]);
-            findSubSetsOfLength(nums,  result, i+1, max, current);
-            current.removeLast();
-        }
-        
-        
+        //take the number
+        findSubSetsOfLength(nums, result, start+1, current);
+        current.addLast(nums[start]);
+        findSubSetsOfLength(nums, result, start+1, current);
+        current.removeLast();
+
     }
+    
+//     public void findSubSetsOfLength1(int nums[], List<List<Integer>> result, int start, int max, LinkedList<Integer> current){
+        
+//         if(current.size()==max){
+//             result.add(new ArrayList<>(current));
+//             return;
+//         }
+        
+//         for(int i=start; i< nums.length;i++) {
+//             current.addLast(nums[i]);
+//             findSubSetsOfLength(nums,  result, i+1, max, current);
+//             current.removeLast();
+//         }
+        
+        
+    // }
 }
