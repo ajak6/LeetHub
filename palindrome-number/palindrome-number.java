5e24b7dd0f73d4272016df9111704eabe5a38df6
Amey Jain <ajak6@users.noreply.github.com>
2021-08-21 02:09:57 -0700

    Time: 7 ms (66.42%), Space: 38.6 MB (32.55%) - LeetHub

--- palindrome-number/palindrome-number.java
+++ palindrome-number/palindrome-number.java
@@ -1,6 +1,8 @@
 class Solution {
     public boolean isPalindrome(int x) {
        //create  the reverse number
+        //reversing the number is risky because we may overflow the integer  but in such cases asnwer will anyways be false becuase the same number should e given as input which is alreadhy overflowing.
+        // reversing half number? 
         int sum =0;
         if(x<0)return false;
         int original = x;
