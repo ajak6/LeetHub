656978f4ba01f7e8b98775ee71c5d32800727f3b
Amey Jain <ajak6@users.noreply.github.com>
2021-08-27 00:12:08 -0700

    Time: 75 ms (7.69%), Space: 65.2 MB (6.60%) - LeetHub

--- implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -1,69 +1,54 @@
-class TrieNode{
-    char val;
-    TrieNode[] nodes;
+class TrieNode {
+    TrieNode children [];
     boolean isWord;
+    char val;
     public TrieNode(){
-        nodes = new TrieNode [26];
-    }
-    public TrieNode getNode(char c){
-        return nodes[c-'a'];
-    }
-    public TrieNode addNode(char c){
-        if(!isPresent(c)){
-            nodes[c-'a']=new TrieNode();
-        }
-        return nodes[c - 'a'];
-    }
-    public boolean isPresent(char c){
-        return nodes[c-'a']!=null;
+        val=' ';
+        children = new TrieNode[26];
     }
 }
+
 class Trie {
-    // Map<Character, Trie> nodes;
     TrieNode root;
-    ;
     /** Initialize your data structure here. */
     public Trie() {
-        root = new TrieNode(); 
-        //let this be the root node which stores the trieNode for first character
+        root = new TrieNode();
     }
     
     /** Inserts a word into the trie. */
+    // abcd
     public void insert(String word) {
-        TrieNode tempRoot = root;
-        for (int i=0; i < word.length(); i++) {
+        TrieNode node = root;
+        for(int i =0; i < word.length();i++) {
             char c = word.charAt(i);
-            tempRoot = tempRoot.addNode(c);
+            if(node.children[c-'a'] == null){
+                node.children[c-'a']= new TrieNode();
+            } 
+            node = node.children[c-'a'];
         }
-        tempRoot.isWord=true;
+        node.isWord = true;
     }
     
     /** Returns if the word is in the trie. */
     public boolean search(String word) {
-        TrieNode tempRoot = root;
-       for (int i=0; i < word.length(); i++) {
-            char c= word.charAt(i);
-            if(tempRoot.isPresent(c)){
-              tempRoot=tempRoot.getNode(c);  
-            } else{
-                return false;    
-            }
-       }
-        return tempRoot.isWord;
+        TrieNode node = root;
+        for(char c: word.toCharArray()){
+            
+            node=node.children[c-'a'];
+            if(node==null)return false;
+        }
+        return node.isWord;
     }
     
     /** Returns if there is any word in the trie that starts with the given prefix. */
     public boolean startsWith(String prefix) {
-        TrieNode tempRoot = root;
-        for (int i=0; i < prefix.length(); i++) {
-            char c = prefix.charAt(i);
-            if(tempRoot.isPresent(c)){
-                tempRoot=tempRoot.getNode(c);
-            } else{
-                return false;
-            }    
+            TrieNode node =root;
+        for (char c: prefix.toCharArray()) {
+            if(node==null)return false;
+            node=node.children[c-'a'];
         }
-        return tempRoot!=null;
+        return node!=null;
+        
     }
 }
 
