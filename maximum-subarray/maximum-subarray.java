be41f1d058e91596eaea96a477624f8d560bdbd4
Amey Jain <ajak6@users.noreply.github.com>
2021-09-13 23:07:11 -0700

    Time: 162 ms (5.12%), Space: 41.4 MB (14.42%) - LeetHub

---
+++ maximum-subarray/maximum-subarray.java
@@ -0,0 +1,66 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        // [-2,1,-2,4,-1,2,1,-5,4] => [-2, -1, -3, 1, 0, 2, 3, -2, 2]
+        int sum[] = new int[nums.length+1];
+        for(int i=0; i < nums.length ; i++){
+            sum[i+1] = sum[i]+nums[i];
+        }
+        // System.out.println(Arrays.toString(sum));
+        int max = Integer.MIN_VALUE;
+        for(int i=0 ; i  < sum.length; i++) {
+            
+            for(int j=i+1; j < sum.length ; j++) {
+                max = Math.max(max, sum[j]-sum[i]);
+            }
+        }
+        return max;
+    }
+    
+    
+    public int maxSub2Array(int[] nums) {
+        int sum [] = new int[nums.length+1];
+        // int maxSum = Integer.MIN_VALUE;
+        sum[0]=0;
+        //what if you just track the lowest sum till you have seen and keep taking the max between new sums you are seeing?
+        int minsum=0;
+        for (int i=1; i<sum.length; i++) {
+            sum[i]=sum[i-1]+nums[i-1];
+            // minsum=Math.min(minsum, sum[i]);
+            // maxSum=Math.max(minsum, sum[i]-minsum);
+            
+        }
+        
+        // now sum array stores all your sum up until a location.it is possible that the sums are negative.
+        // so you can do 2 pointers on this sub array 
+        // sum[right]-sum[left];
+        /*
+        
+        you increase right
+        for each elft keep increasing right and take differenfce so that you find the sum at each and find the max
+        
+        if for each element we keep taking the diff then 1--2 wil be 3 but sun is -1
+        */
+        
+        // int maxSum=sum[0];
+        // System.out.println(Arrays.toString(sum));
+        // same as buy and sell stock problem. where you just maintain the lowest sum you have seen
+        //keep taking the diff with the sum till here.
+        //minSum= 0initial value means sum includes all element start from 0 in original array. if we have a negative sum
+        //it means we will
+        int maxSum=Integer.MIN_VALUE;
+        int minSum = sum[0]; // 0
+        for (int i=1; i<sum.length; i++) {            
+
+            maxSum=Math.max(maxSum, sum[i]-minSum);
+            minSum  = Math.min(minSum, sum[i]);
+        
+        }
+        return maxSum;
+        /* optimize */
+        
+        
+        
+        
+        
+    }
+}
