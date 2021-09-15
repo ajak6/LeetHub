352636d916aaab47c2e299c48b7864ce9ec51ccb
Amey Jain <ajak6@users.noreply.github.com>
2021-09-14 22:21:26 -0700

    Time: 2 ms (49.59%), Space: 51.8 MB (8.22%) - LeetHub

--- number-of-provinces/number-of-provinces.java
+++ number-of-provinces/number-of-provinces.java
@@ -1,85 +1,82 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        //brute force : using adj matrix : yiou
-        //union find 
-        //datastructure which stores the parent of each node
-        // at first everyone is their own parent.
-        //when there is a connection you mark the other as their parent
-        //when you see a connection between 
-//         UnionFind union = new UnionFind(isConnected);
-//         for(int i=0; i < isConnected.length ; i++){
-//             //if you start at j you are calling union i,j and then later with j,i which will affect the union datastructure/
-//                 // if(isConnected[i][j]==1)
-//                     union.parent.put(i,i);
-            
-            
+        int max = isConnected.length;
+//         int roots []  = new int[max];
+        
+//         for(int i=0; i < roots.length ; i++){
+//             roots[i]=i;
 //         }
-//         for(int i=0; i < isConnected.length ; i++){
-//             //if you start at j you are calling union i,j and then later with j,i which will affect the union datastructure/
-//             for(int j = 0; j < isConnected[i].length; j++){
-//                 if(isConnected[i][j]==1)
-//                     union.union(i,j);
-//                     System.out.println(union.parent);        
+        
+//         for(int i=0; i < roots.length ; i++){
+//             for(int j = i+1; j < roots.length; j++){
+                
+//                 if(isConnected[i][j]==1) union(roots, i, j);
 //             }
 //         }
-//         //at the end we need to find the total unique parent in unionFind.
-//         System.out.println(union.parents());
-//         int answer = union.parents().size();
-//         return answer;
-        int [] root = new int[isConnected.length];
-        for(int i=0 ; i < root.length ; i++) root[i]=i;
+//         // System.out.println(Arrays.toString(roots));
         
-        for(int i=0; i < root.length ; i++){
-            for(int j = i+1; j < root.length ; j++){
-                
-                if(isConnected[i][j]==1){
-                    unionFind(root, i,j);
-                }
+//         int count =0;
+//         for(int i=0; i < roots.length ; i++){
+//             if(i==roots[i]) count++;
+//         }
+//         return count;
+        
+        UnionFind finder = new UnionFind(max);
+        for(int i = 0 ; i < isConnected.length; i++){
+            for(int j = i+1; j < isConnected.length ; j++){
+                if(isConnected[i][j]==1) finder.union(i,j);
             }
         }
-        int count=0;
-        for (int i = 0; i < root.length; i++)
-            if (i == root[i]) count++;
-        return count;
+        int count =0;
+        // System.out.println(Arrays.toString(finder.roots));
+        // for(int i=0; i < finder.roots.length ; i++){
+        //     if(finder.roots[i]==i) count++;
+        // }
+        return finder.total;
     }
     
-    void unionFind (int[] root, int v1, int v2) {
-        while (root[v1] != v1) v1 = root[v1]; //find v1's root
-        while (root[v2] != v2) v2 = root[v2]; //find v2's root
-        if (root[v1] != root[v2]) root[v2] = v1; //unite the 2 subtrees 
+    public void union(int roots[], int i, int j){
+        
+        //LOOP UNTIL YOU FIND THE THE PARENT OF I WHICH IS I
+        while(roots[i]!=i) i = roots[i];
+        while(roots[j]!=j) j = roots[j];
+        if(i==j) return;   
+        roots[j] = i;
     }
-    
-    
 }
 class UnionFind {
-    Map<Integer,Integer> rank = new HashMap<>();
-    //bascially we need to find the union with unique parents 
-    // total number of parents at the end will bne the answer
-    public UnionFind(int graph[][]){
-        // for(int i=0; i < graph.length ; i++) {
-        //     parent.put(i,i);
-        // }
+    public void union(int i, int j){
+        // int parA = find(i);
+        // while(roots[i]!=i) i = roots[i];
+        // while(roots[j]!=j) j = roots[j];
+        i = find(i);
+        j = find(j);
+        // int parB = find(j);
+        // if(parA==parB) return;
+        // total--;
+        // roots[j]=parA;
+        if(i==j) return;  
+        total--;
+        roots[j]=i;
     }
-    
-    public Set<Integer> parents(){
-        return new HashSet<>(parent.values());
-    }
-    
-    HashMap<Integer, Integer> parent = new HashMap<>();
-    // initialize each with ittself as the parent
-    public int union(int a, int b){
-        int parentOfA = find(a);
-        int parentOfB = find(b);
-        if(parentOfA == parentOfB)return parentOfA;
-        parent.put(b, parentOfA);
-        return parentOfA;
-    }
-    
-    public int find(int a){
-        if(parent.get(a)!=a){
-            return find(parent.get(a));
+    int roots[];
+    int total;
+    public UnionFind(int n) {
+        roots = new int[n];
+        for(int i=0; i < n ; i++){
+            roots[i]=i;
         }
-        return a;
+        total = n;
     }
-     
+    
+    
+    
+    public int find(int i) {
+        if(roots[i]!=i){
+            // roots[i] = roots[roots[i]];
+            i = roots[i];
+            return find(i);
+        }
+        return i;
+    }
 }
