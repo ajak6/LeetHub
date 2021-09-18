a081f31f9d6cf8c18e3d2c2927937943d2d6b8f1
Amey Jain <ajak6@users.noreply.github.com>
2021-09-18 15:35:36 -0700

    Time: 5 ms (90.33%), Space: 39.1 MB (81.56%) - LeetHub

--- basic-calculator/basic-calculator.java
+++ basic-calculator/basic-calculator.java
@@ -1,5 +1,62 @@
 class Solution {
     public int calculate(String s) {
+        //edge case: witghout worry abojut paranthesis this case will fail -(3+(4+5))
+        //
+        // traverse and look at each character
+        // continue if blank
+        // if number continue to create the number
+        // () and (-) will be there
+        // evaluate whats in () first
+        // since this is only + and - we can ignore ( and continue to check the sign and number fir now)
+        //once you have a number add it to a sum value and start create a new number
+        //store the sign of the numebr if you hit - and then just use it to add ot substract
+        int result = 0;
+        int sign = 1;
+        int runningNumber = 0;
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0; i < s.length(); i++) {
+            //12+(-1)
+            char c = s.charAt(i);
+            if(c>='0' && c <= '9'){
+                runningNumber=runningNumber*10+ (c-'0');
+            } else if(c=='+'||c=='-') {
+                //running number is done
+                result += (runningNumber)*sign;
+                runningNumber=0;
+                sign = c=='-' ? -1:1;
+            }
+            if(c=='('){
+                //we need to recurse and get the remaining result
+                stack.push(result);
+                stack.push(sign);
+                runningNumber =0;
+                result=0;
+                sign = 1;
+                    
+            }
+            if(c==')'){
+                //this recursive call has ended and should return the result whatever it has to caller
+                
+                // result*sign+runningNUmber = in case of (-1)  this will fail. we will loose the sign since result will be 0;
+                // 4+(-5) => 4 to stack, +1 to stack=> then you hit -1 sign, result =0, sign=-1, hit 5 runningNumber is 5, then hig ) 0+-1*5=result =-5, old result=4+(old sign)* (result) = 4 +1*-5 = -1
+                result = result + sign*runningNumber; 
+                int oldSign = stack.pop();
+                int oldResult = stack.pop();
+                result = oldResult + result * oldSign;
+                runningNumber=0;
+                
+            }
+            if(i==s.length()-1){
+                //last char
+                result += (runningNumber)*sign;
+            }
+        }
+        // At the end if we hit a ' ' or ')' then we can't really 
+        // the running number is lingering without any value.
+        return result;
+    }
+    
+    public int calculate1(String s) {
         
         // will the numbers be decimal always? no exponents?
         // normal expresssion
@@ -50,7 +107,6 @@ class Solution {
                     sign = 1;
                 }
                 number=0;
-                
             }
             
         }
