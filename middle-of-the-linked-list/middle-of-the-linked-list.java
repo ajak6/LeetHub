c9e5f56d687d0870f5615608e4d8890f10e64f62
Amey Jain <ajak6@users.noreply.github.com>
2021-09-21 23:20:46 -0700

    Time: 0 ms (100.00%), Space: 36.6 MB (58.33%) - LeetHub

---
+++ middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        //what doe odd lenght and even length?
+        //1->2>3>4 3
+        
+        //1>2>3>4>5 -> 3
+    //    s s s
+//        f   f   f
+              
+        ListNode slow=head;
+        ListNode fast=head;
+        
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        
+        return slow;
+        
+    }
+}
