e7b34b5073c6b6d44229520d7ce386dd093bda40
Amey Jain <ajak6@users.noreply.github.com>
2021-10-10 00:42:46 -0700

    Time: 0 ms (100.00%), Space: 37.1 MB (75.90%) - LeetHub

--- minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -1,40 +1,58 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-        //minimum moves optimization problem.
-        //so that entire string become valid.
-        //())- if you find invalid indexes from left you know 1 invalid index is present add 1 corresponding to thaht
-        //"()))((" -> 
-        int open = 0;
-        char c[] = s.toCharArray();
-        int invalid =0;
-        for(char ch: c){
-            
-            if(ch == '(' ) {
-                open++;
-            } else { 
-                if(open==0){
-                    invalid++;
-                }else{
-                    open--;
-                }
+        //onyl para
+        // can we move para in teh strting )( does it ean we can rearrrange?
+        int open=0;
+        int close=0;
+        //traverse count open
+        // reduce if close ifs found and open is present
+        //else count invalid closed
+        //at the end open + close
+        for(int i=0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c=='(') open++;
+            if(c==')'){
+                if(open>0)open--;
+                else close++;
             }
         }
-        int close =0;
-        for(int i=c.length-1; i>=0; i--) {
+        return open+close;
+        
+//         //minimum moves optimization problem.
+//         //so that entire string become valid.
+//         //())- if you find invalid indexes from left you know 1 invalid index is present add 1 corresponding to thaht
+//         //"()))((" -> 
+//         int open = 0;
+//         char c[] = s.toCharArray();
+//         int invalid =0;
+//         for(char ch: c){
+            
+//             if(ch == '(' ) {
+//                 open++;
+//             } else { 
+//                 if(open==0){
+//                     invalid++;
+//                 }else{
+//                     open--;
+//                 }
+//             }
+//         }
+//         int close =0;
+//         for(int i=c.length-1; i>=0; i--) {
             
             
-            if(c[i] == ')' ) {
-                close++;
-            } else { 
-                if(close==0){
-                    invalid++;
-                }else{
-                    close--;
-                }
-            }
-        }
+//             if(c[i] == ')' ) {
+//                 close++;
+//             } else { 
+//                 if(close==0){
+//                     invalid++;
+//                 }else{
+//                     close--;
+//                 }
+//             }
+//         }
 
-        return invalid;
+//         return invalid;
         
     }
 }
